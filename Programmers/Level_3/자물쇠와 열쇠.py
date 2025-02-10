f1f974a7022ba3415cfb1dbba1a6588338d65356
eunaJung01 <christinejung10@daum.net>
2025-02-10 20:54:28 +0900

    add: Programmers - 가운데 글자 가져오기

---
+++ Programmers/Level_3/자물쇠와 열쇠.py
@@ -0,0 +1,8 @@
+def solution(key, lock):
+    # 핵 구현 문제인듯 ;;
+    # key 돌기 & lock 홈
+    # 0. key 돌기 개수 & lock 홈 개수 세기
+    # 1. 1ㄷ1로 매칭, 90도씩 돌려가면서 ㄱㄱ
+    # 2. key의 모든 돌기와 lock의 모든 홈이 맞춰졌는지 확인 (맞춰진 개수가 돌기 개수 & 홈 개수와 동일한가?)
+    answer = True
+    return answer
