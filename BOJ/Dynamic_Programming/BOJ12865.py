749472b6b3276159ed0f1a6c490a9bb623b05ef1
eunaJung01 <christinejung10@daum.net>
2025-02-28 17:21:44 +0900

    fix: BOJ12865

--- BOJ/Dynamic_Programming/BOJ12865.py
+++ BOJ/Dynamic_Programming/BOJ12865.py
@@ -1,23 +1,24 @@
-# 평범한 배낭
-
 import sys
 
-N, K = map(int, sys.stdin.readline().split())  # 물품의 수, 버틸 수 있는 무게
+input = sys.stdin.readline
 
-W = []  # 각 물건의 무게
-V = []  # 각 물건의 가치
+N, K = map(int, input().split())
+W = []
+V = []
 for _ in range(N):
-    w, v = map(int, sys.stdin.readline().split())
+    w, v = map(int, input().split())
     W.append(w)
     V.append(v)
 
 dp = [[0 for _ in range(K + 1)] for _ in range(N + 1)]
 
-for i in range(1, N + 1):
-    for j in range(1, K + 1):
-        if W[i - 1] <= j:
-            dp[i][j] = max(dp[i - 1][j], V[i - 1] + dp[i - 1][j - W[i - 1]])
+for n in range(1, N + 1):
+    for k in range(1, K + 1):
+        w = W[n - 1]
+        v = V[n - 1]
+        if w > k:
+            dp[n][k] = dp[n - 1][k]
         else:
-            dp[i][j] = dp[i - 1][j]
+            dp[n][k] = max(dp[n - 1][k], dp[n - 1][k - w] + v)
 
 print(dp[N][K])
