79e367a4633241c7116c6971a2ae493333dd272d
eunaJung01 <christinejung10@daum.net>
2025-03-01 11:41:43 +0900

    fix: BOJ1753

--- BOJ/Dijkstra/BOJ1753.py
+++ BOJ/Dijkstra/BOJ1753.py
@@ -1,41 +1,32 @@
-# 최단경로
-
-import sys
 import heapq
+import sys
 
 input = sys.stdin.readline
 
-V, E = map(int, input().split())
-start_node = int(input().strip())
+INF = int(1e9)
 
-graph = [[] for _ in range(V + 1)]
+V, E = map(int, input().split())
+K = int(input())
+edges = [[] for _ in range(V + 1)]
 for _ in range(E):
     u, v, w = map(int, input().split())
-    graph[u].append([v, w])
+    edges[u].append((v, w))
 
-INF = 2147000000
+heap = []
+heapq.heappush(heap, (0, K))
+
 dp = [INF for _ in range(V + 1)]
+dp[K] = 0
 
+while heap:
+    _, node = heapq.heappop(heap)
+    for next_node, weight in edges[node]:
+        if dp[next_node] > dp[node] + weight:
+            dp[next_node] = dp[node] + weight
+            heapq.heappush(heap, (dp[next_node], next_node))
 
-def dijkstra(start):
-    dp[start] = 0
-    heap = []
-    heapq.heappush(heap, (0, start))
-
-    while heap:
-        u_w, u = heapq.heappop(heap)
-        if dp[u] < u_w:
-            continue
-        for v, v_w in graph[u]:
-            new_w = dp[u] + v_w
-            if dp[v] > new_w:
-                dp[v] = new_w
-                heapq.heappush(heap, (new_w, v))
-
-
-dijkstra(start_node)
 for i in range(1, V + 1):
     if dp[i] == INF:
         print("INF")
-    else:
-        print(dp[i])
+        continue
+    print(dp[i])
