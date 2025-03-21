391a1aa05a9371ae97dbaf673d6af11efe9d0256
eunaJung01 <christinejung10@daum.net>
2025-03-21 15:50:12 +0900

    fix: BOJ1202

--- BOJ/Greedy/BOJ1202.py
+++ BOJ/Greedy/BOJ1202.py
@@ -1,24 +1,28 @@
-# 보석 도둑
-
 import sys
 import heapq
 
 input = sys.stdin.readline
 
-N, K = map(int, input().split())
+N, K = map(int, input().split())  # 보석 개수, 가방 개수
 
-jewels = []  # 우선순위 큐 : 무게 낮은 순
+jewels = []
 for _ in range(N):
-    heapq.heappush(jewels, list(map(int, input().split())))
+    m, v = map(int, input().split())  # 무게, 가격
+    heapq.heappush(jewels, (m, v))
 
-bags = [int(input().strip()) for _ in range(K)]
+bags = []
+for _ in range(K):
+    c = int(input().strip())  # 담을 수 있는 최대 무게
+    bags.append(c)
 bags.sort()
 
-steal = 0
-costs = []  # 우선순위 큐 : 가격 높은 순
+answer = 0
+candidate_prices = []
 for bag in bags:
     while jewels and jewels[0][0] <= bag:
-        heapq.heappush(costs, -heapq.heappop(jewels)[1])
-    if costs:
-        steal += abs(heapq.heappop(costs))
-print(steal)
+        m, v = heapq.heappop(jewels)
+        heapq.heappush(candidate_prices, -v)
+    if len(candidate_prices) > 0:
+        max_price = -heapq.heappop(candidate_prices)
+        answer += max_price
+print(answer)
